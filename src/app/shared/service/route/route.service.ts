7fded635741fa948c9e24a89d55a504014968960
sztorma <sztorma@outlook.com>
2022-04-20 17:23:55 +0200

    route service api call

--- src/app/shared/service/route/route.service.ts
+++ src/app/shared/service/route/route.service.ts
@@ -1,16 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Route } from '../../model/route.model';
 import { IRouteService } from './iroute.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteService implements IRouteService {
 
-  constructor() { }
+  apiUrl = environment.apiUrl;
+
+  constructor(private Http: HttpClient) { }
+
   getRoutesForComponent(component: string): Observable<Route[]> {
-    throw new Error('Method not implemented.');
+    return this.Http.get<Route[]>(this.apiUrl + '/api/route/get/for-current-roles/for-component/' + component, httpOptions);
   }
 
 }
