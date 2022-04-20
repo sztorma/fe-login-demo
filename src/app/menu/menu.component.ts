9db951282c5ffdc6e0e5b616e6de534b71503428
sztorma <sztorma@outlook.com>
2022-04-20 09:20:25 +0200

    display constant routes

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Route } from '../shared/model/route.model';
 import { User } from '../shared/model/user.model';
 import { StorageService } from '../shared/service/auth/storage.service';
 import { IUserService } from './service/user/iuser.service';
@@ -12,6 +13,29 @@ import { IUserService } from './service/user/iuser.service';
 export class MenuComponent implements OnInit {
 
   user: User = new User;
+  routes: Route[] = [
+    {
+      id: 11,
+      component: 'menu-nav',
+      order: 1,
+      name: 'admin',
+      link: '/menu/admin'
+    },
+    {
+      id: 22,
+      component: 'menu-nav',
+      order: 2,
+      name: 'moderator',
+      link: '/menu/moderator'
+    },
+    {
+      id: 33,
+      component: 'menu-nav',
+      order: 3,
+      name: 'user',
+      link: '/menu/user'
+    }
+  ]
 
   constructor(private router: Router, private storageService: StorageService, @Inject('IUserService') private userService: IUserService) { }
 
---
+++ src/app/shared/model/route.model.ts
@@ -0,0 +1,7 @@
+export class Route {
+    id!: number;
+    component!: string;
+    order!: number
+    name!: string;
+    link!: string;
+}
