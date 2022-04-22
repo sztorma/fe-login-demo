e1fbb42d49a2c19dee7af5fe9da445777e507cc3
sztorma <sztorma@outlook.com>
2022-04-22 11:52:04 +0200

    moderator content for api call

--- src/app/menu/user-content/user-content.component.ts
+++ src/app/menu/user-content/user-content.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserContentComponent implements OnInit {
 
+  path: string = '/api/content/user';
+
   constructor() { }
 
   ngOnInit(): void {
