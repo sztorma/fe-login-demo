8dfdfbeedbef6c18390a6fa19f2260f3cf6d8c28
sztorma <sztorma@outlook.com>
2022-04-22 13:26:19 +0200

    mock content service

--- src/app/menu/service/content/mock-content.service.ts
+++ src/app/menu/service/content/mock-content.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Content } from 'src/app/shared/model/content.model';
 import { IContentService } from './icontent.service';
 
 @Injectable({
@@ -8,9 +9,10 @@ import { IContentService } from './icontent.service';
 export class MockContentService implements IContentService {
 
   constructor() { }
-
-  getAdminContent(path: string): Observable<string> {
-    return of('mock admin content from path: ' + path);
+  getContent(path: string): Observable<Content> {
+    let content: Content = new Content;
+    content.response = 'mock admin content from path: ' + path;
+    return of(content);
   }
 
 }
